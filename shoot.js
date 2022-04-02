414d0a08e7b9c1e9e80e0d5e0d3e03fa71189c8c
sandeeppunmia <67103020+sandeeppunmia@users.noreply.github.com>
2022-04-02 17:11:13 +0530

    Add files via upload

---
+++ shoot.js
@@ -0,0 +1,53 @@
+AFRAME.registerComponent("bullets",{
+    init:function(){
+        this.shootBullet();
+    },
+    shootBullet:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(e.key==="z"){
+                var bullet=document.createElement("a-entity")
+                bullet.setAttribute("geometry",{
+                    primitive:"sphere",
+                    radius:0.1
+                })
+                bullet.setAttribute("material","color","black")
+                var cam = document.querySelector("#camera")
+                pos = cam.getAttribute("position")
+                bullet.setAttribute("position",{
+                    x:pos.x,
+                    y:pos.y,
+                    z:pos.z
+                })
+                var camera = document.querySelector("#camera").object3D
+                var direction = new THREE.Vector3()
+                camera.getWorldDirection(direction)
+                bullet.setAttribute("velocity",direction.multiplyScalar(-10))
+                bullet.setAttribute("dynamic-body",{shape:"sphere",mass:"0"})
+                bullet.addEventListener("collide",this.removeBullet)
+                var scene=document.querySelector("#scene")
+                scene.appendChild(bullet)
+            }
+        })
+    },
+    removeBullet:function(e){
+        console.log(e.detail.target.el);
+        console.log(e.detail.body.el);
+        var element = e.detail.target.el;
+        var elementHit = e.detail.body.el;
+        
+        if(elementHit.id.includes("box")){
+            elementHit.setAttribute("material",{
+                opacity:1,
+                transparent:true
+            })
+            var impulse = new CANNON.Vec3(-2,2,1)      
+            var worldPoint = new CANNON.Vec3().copy(
+                elementHit.getAttribute("position")
+            ) 
+            elementHit.body.applyImpulse(impulse,worldPoint) //var,direction
+            element.removeEventListener("collide",this.shoot)
+            var scene = document.querySelector("#scene")
+            scene.removeChild(element)
+        }
+    }
+})
